04ac15919268541512e3d19fedaea91ec593df3a
Amy <amyannnwong@gmail.com>
2022-09-20 14:09:48 -0400

    matching and flipping cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import "./App.css";
 import Card from "./components/Card";
 
 const cardImages = [
-  { src: "/img/gold.jpg" },
-  { src: "/img/green-blue.jpg" },
-  { src: "/img/light-blue.jpg" },
-  { src: "/img/pink.jpeg" },
-  { src: "/img/purple.jpg" },
-  { src: "/img/red.jpg" },
+  { src: "/img/gold.jpg", matched: false },
+  { src: "/img/green-blue.jpg", matched: false },
+  { src: "/img/light-blue.jpg", matched: false },
+  { src: "/img/pink.jpeg", matched: false },
+  { src: "/img/purple.jpg", matched: false },
+  { src: "/img/red.jpg", matched: false },
 ];
 
 function App() {
@@ -23,39 +23,48 @@ function App() {
       .map((card) => ({ ...card, id: Math.random() }));
 
     setCards(shuffledCards);
-    setTurns(0)
+    setTurns(0);
   };
 
   const handleChoice = (card) => {
-    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
-  }
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
 
   // compare two selected cards; useEffect fires when the component first mounts and then again when dependency changes
   useEffect(() => {
     if (choiceOne && choiceTwo) {
       if (choiceOne.src === choiceTwo.src) {
-        console.log('those cards match')
+        setCards((previousCards) => {
+          return previousCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
         resetTurn();
       } else {
-        console.log('those cards don\'t match')
-        resetTurn();
+        setTimeout(() => resetTurn(), 1000);
       }
     }
-  }, [choiceOne, choiceTwo])
+  }, [choiceOne, choiceTwo]);
+
+  console.log("cards: ", cards);
 
   const resetTurn = () => {
-    setChoiceOne(null)
-    setChoiceTwo(null)
-    setTurns(previousTurns => previousTurns + 1)
-  }
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((previousTurns) => previousTurns + 1);
+  };
 
   return (
     <div className="App">
-      <h1>Memory Game</h1>
+      <h1>React Crystal Memory Game</h1>
       <button onClick={shuffleCards}>New Game</button>
       <div className="card-grid">
-        {cards.map(card => (
-          <Card key={card.id} card={card} handleChoice={handleChoice} />
+        {cards.map((card) => (
+          <Card key={card.id} card={card} handleChoice={handleChoice} flipped={card === choiceOne || card === choiceTwo || card.matched} />
         ))}
       </div>
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,14 @@
 import "./Card.css"
 
-export default function Card ({ card, handleChoice }) {
+export default function Card ({ card, handleChoice, flipped }) {
   const handleClick = () => {
     handleChoice(card)
   }
-  
+
   return (
     <div>
       <div className="individual-card">
-            <div>
+            <div className={flipped ? "flipped" : ""}>
               <img className="front" src={card.src} alt="front of card" />
               <img className="back" src="/img/cover.jpg" onClick={handleClick} alt="back of card" />
             </div>
